64cf4edba5163bf79d0d438bf13ff554420511b1
Panagiotis Kapsalis <pkapsalis@epu.ntua.gr>
2020-06-03 21:14:44 +0300

    some changes on load.sh

--- Scripts/load_data.sh
+++ Scripts/load_data.sh
@@ -1,5 +1,11 @@
+# Create Data Directory
+mkdir $HOME/project-data
+
 # Install unzip
 sudo apt-get install -y unzip
+
+# Go to project-data directory
+cd $HOME/project-data
 
 # Get taxi data
 wget http://www.cslab.ntua.gr/courses/atds/yellow_trip_data.zip
