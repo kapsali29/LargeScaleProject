5e27863ab628ee2a01102311a5e73c1e96c97956
Panagiotis Kapsalis <pkapsalis@epu.ntua.gr>
2020-06-03 21:34:57 +0300

    Some fixes on settings

--- settings.py
+++ settings.py
@@ -2,5 +2,5 @@ import os
 
 # APPLICATION SETTINGS
 YELLOW_TRIP_DATA = '/home/user/project-data/yellow_trip_data'
-TRIP_DATA = os.path.join(YELLOW_TRIP_DATA, 'yellow_tripdata_1m.csv ')
+TRIP_DATA = os.path.join(YELLOW_TRIP_DATA, 'yellow_tripdata_1m.csv')
 TRIP_VENDORS = os.path.join(YELLOW_TRIP_DATA, 'yellow_tripvendors_1m.csv')
