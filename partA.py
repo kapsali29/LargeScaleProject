d9d2419d1aa82ca31652b95533629f069ebd023d
Panagiotis Kapsalis <pkapsalis@epu.ntua.gr>
2020-06-03 21:30:08 +0300

    some changes on settings

--- partA.py
+++ partA.py
@@ -1,3 +1,5 @@
-from helpers import init_spark
+from helpers import init_spark, load_csv_data
+from settings import TRIP_DATA
 
 spark, sc = init_spark()
+trip_data = load_csv_data(spark, TRIP_DATA)
