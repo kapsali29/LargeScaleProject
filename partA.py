03f5ce8ae9ac608dbf1a56412035312e4921328f
Panagiotis Kapsalis <pkapsalis@epu.ntua.gr>
2020-06-05 15:40:20 +0300

    some changes on partA

--- partA.py
+++ partA.py
@@ -9,8 +9,8 @@ trip_data = load_csv_data(spark, TRIP_DATA)
 
 # write_df_to_hdfs(trip_data, HDFS_TRIP_DATA_PATH)
 
-trip_data = trip_data.dropDuplicates()
-trip_data = trip_data.na.drop()
+# trip_data = trip_data.dropDuplicates()
+# trip_data = trip_data.na.drop()
 # Remove dirty
 filtered_df = trip_data.filter(trip_data['_c3'] != '0').filter(trip_data['_c4'] != '0').filter(
     trip_data['_c5'] != '0').filter(trip_data['_c6'] != '0')
@@ -18,7 +18,8 @@ filtered_df = trip_data.filter(trip_data['_c3'] != '0').filter(trip_data['_c4'] 
 # Transform Hour
 get_hour_from_date = udf(lambda x: x.split()[1][:2])
 trips_df = filtered_df.withColumn('_c1', get_hour_from_date(col('_c1')))
-trips_df.show()
+trips_partition = trips_df.select('_c1', '_c3', '_c4')
+trips_partition.show()
 
 # cast to Coordinates to double
 # trip_data = trip_data.withColumn("_c3", trip_data["_c3"].cast("double"))
