c606716ecd66e656c51c160c50f169a2e285d0ec
Panagiotis Kapsalis <pkapsalis@epu.ntua.gr>
2020-06-05 19:10:20 +0300

    some changes on Q1

--- part1A/Q1.py
+++ part1A/Q1.py
@@ -1,3 +1,5 @@
+from pyspark import Row
+
 from helpers import init_spark, date_to_hour
 
 
@@ -9,7 +11,7 @@ class Q1(object):
 
     @staticmethod
     def sql_api(trip_data):
-        """This function is used to execute Q1 query using SparkSQL"""
+        """This function is used to execute Q1 using SparkSQL"""
 
         # Remove dirty rows
         filtered_df = trip_data.filter(trip_data['_c3'] != '0').filter(trip_data['_c4'] != '0').filter(
@@ -25,3 +27,16 @@ class Q1(object):
         # Find avg Lat Lon for each Hour
         q1 = trips_df.groupBy('Hour').avg().orderBy('Hour', ascending=True)
         q1.show(24)
+
+    @staticmethod
+    def mr_api(trip_rdd):
+        """This function is used to execute Q1 using RDD"""
+
+        trip_rdd = trip_rdd.map(lambda row: (Row(Hour=row._c1.split()[1][:2], Latitude=row._c3, Longitude=row._c4)))
+        filtered_rdd = trip_rdd.filter(lambda row: (row.Latitude != '0') & (row.Longitude != '0'))
+        convert_to_float = trip_rdd.map(
+            lambda row: Row(Hour=row.Hour, Latitude=float(row.Latitude), Longitude=float(row.Longitude)))
+
+        convert_to_float.reduceByKey(lambda a, b: (a[0] + b[0], a[1] + b[1], a[2] + b[2]))
+
+
