e31604521fc4bb879c4b837c76e3c9589f93d86c
Panagiotis Kapsalis <pkapsalis@epu.ntua.gr>
2020-06-06 10:13:38 +0300

    some changes on Q1 MR

--- part1A/Q1.py
+++ part1A/Q1.py
@@ -32,13 +32,20 @@ class Q1(object):
     def mr_api(trip_rdd):
         """This function is used to execute Q1 using RDD"""
 
+        # Keep necessary data
         trip_rdd = trip_rdd.map(lambda row: (Row(Hour=row._c1.split()[1][:2], Latitude=row._c3, Longitude=row._c4)))
+
+        # Remove dirty rows
         filtered_rdd = trip_rdd.filter(lambda row: (row.Latitude != '0') & (row.Longitude != '0'))
+
+        # Convert coords to Float
         convert_to_float = filtered_rdd.map(
             lambda row: Row(Hour=row.Hour, Latitude=float(row.Latitude), Longitude=float(row.Longitude)))
 
-        keys = convert_to_float.map(lambda row: (row.Hour, (row.Latitude, row.Longitude, row.Value)))
+        # Transform to (Hour, (Lat, Lon, 1.0))
+        keys = convert_to_float.map(lambda row: (row.Hour, (row.Latitude, row.Longitude, 1.0)))
 
+        # ReduceBy Hour and divide by total appearances
         accumulated = keys.reduceByKey(lambda a, b: (a[0] + b[0], a[1] + b[1], a[2] + b[2]))
         groups = accumulated.map(lambda row: (row[0], row[1][0] / row[1][2], row[1][1] / row[1][2]))
 
