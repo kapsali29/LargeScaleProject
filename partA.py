319a71ebfc8ad344a386b04432ae524e2beaa1bb
Panagiotis Kapsalis <pkapsalis@epu.ntua.gr>
2020-06-06 11:31:42 +0300

    some changes on partA

--- partA.py
+++ partA.py
@@ -1,4 +1,4 @@
-
+from pyspark.sql.types import DoubleType
 
 from helpers import load_csv_data, init_spark, haversine, elapsed_time
 from settings import TRIP_DATA, HDFS_TRIP_DATA_PATH, PARQUET_TRIP_DATA
@@ -15,12 +15,11 @@ trip_data = load_csv_data(spark, TRIP_DATA)
 
 # write_df_to_hdfs(trip_data, HDFS_TRIP_DATA_PATH)
 
-trip_data = trip_data.withColumn("_c3", trip_data["_c3"].cast("double"))
-trip_data = trip_data.withColumn("_c4", trip_data["_c4"].cast("double"))
-trip_data = trip_data.withColumn("_c5", trip_data["_c5"].cast("double"))
-trip_data = trip_data.withColumn("_c6", trip_data["_c6"].cast("double"))
+trip_data = trip_data.withColumn("_c3", trip_data["_c3"].cast(DoubleType()))
+trip_data = trip_data.withColumn("_c4", trip_data["_c4"].cast(DoubleType()))
+trip_data = trip_data.withColumn("_c5", trip_data["_c5"].cast(DoubleType()))
+trip_data = trip_data.withColumn("_c6", trip_data["_c6"].cast(DoubleType()))
 
 
-result = trip_data.rdd.map(haversine).toDF()
-result = result.rdd.map(elapsed_time).toDF()
+result = trip_data.rdd.map(haversine).map(elapsed_time).toDF()
 result.show()
