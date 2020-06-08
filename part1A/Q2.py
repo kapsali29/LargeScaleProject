1c9f6d04e0a8046e5e68755d39d93f4a6574dcd8
Panagiotis Kapsalis <pkapsalis@epu.ntua.gr>
2020-06-08 14:27:52 +0300

    Some changes on Q2

--- part1A/Q2.py
+++ part1A/Q2.py
@@ -7,16 +7,25 @@ class Q2(object):
     def __init__(self):
         self.spark, self.sc = init_spark()
 
-    def sql_api(self, trip_data):
+    def sql_api(self, trip_data, vendors):
         """This function is used to execute Q1 using SparkSQL"""
+
+        trip_data.registerTempTable("trips")
+        vendors.registerTempTable("vendors")
 
         self.spark.udf.register('elapsed_time', elapsed_time)
         self.spark.udf.register('haversine', haversine)
 
-        trip_data.registerTempTable("trips")
-        res = self.spark.sql(
-            "SELECT _c0 as vendor, elapsed_time(_c1, _c2) as duration,haversine(cast(_c3 as float), cast(_c4 as float), cast(_c5 as float), cast(_c6 as float)) as distance from trips")
+        vendor_trips = self.spark.sql(
+            "SELECT vendors._c1 as vendor_id, elapsed_time(trips._c1,trips._c2) as duration, haversine(cast(_c3 as float), cast(_c4 as float), cast(_c5 as float), cast(_c6 as float)) as distance FROM trips INNER JOIN vendors ON trips._c0=vendors._c0 WHERE trips._c3!='0' AND trips._c4!='0' AND trips._c5!='0' AND trips._c6!='0'")
+        vendor_trips.registerTempTable("vendor_trips")
 
-        res.registerTempTable("results")
-        groups = self.spark.sql("SELECT vendor, MAX(distance), MAX(duration) FROM results GROUP BY vendor")
-        groups.show()
+        groups = self.spark.sql("SELECT vendor_id, MAX(distance) as max_distance FROM vendor_trips GROUP BY vendor_id")
+        grp_rdd = groups.rdd.collect()
+
+        result = self.spark.sql(
+            "SELECT * FROM vendor_trips WHERE vendor_id={} AND distance={} OR vendor_id={} AND distance={}".
+            format(grp_rdd[0]['vendor_id'], grp_rdd[0]['max(distance)'], grp_rdd[1]['vendor_id'],
+                   grp_rdd[1]['max(distance)'])
+            )
+        result.show()
--- partA.py
+++ partA.py
@@ -11,7 +11,6 @@ spark, sc = init_spark()
 # q1 = Q1()
 trip_data = load_csv_data(spark, TRIP_DATA)
 
-
 # trip_rdd = trip_data.rdd
 # q1.mr_api(trip_rdd)
 # q1.sql_api(trip_data)
@@ -31,47 +30,67 @@ trip_data = load_csv_data(spark, TRIP_DATA)
 #
 # group.show()
 
-def elapsed_time(start, end):
-    """This function is used to find trip duration"""
-
-    start_date = datetime.strptime(start, '%Y-%m-%d %H:%M:%S')
-    end_date = datetime.strptime(end, '%Y-%m-%d %H:%M:%S')
-
-    duration = end_date - start_date
-    days, seconds = duration.days, duration.seconds
-    minutes = (seconds % 3600) // 60
-    return minutes
-
-
-def haversine(lat1, lon1, lat2, lon2):
-    """
-    Calculate the great circle distance between two points
-    on the earth (specified in decimal degrees)
-    """
-
-    # convert decimal degrees to radians
-    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
-
-    # haversine formula
-    dlon = lon2 - lon1
-    dlat = lat2 - lat1
-    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
-    c = 2 * asin(sqrt(a))
-    r = 6371  # Radius of earth in kilometers. Use 3956 for miles
-
-    distance = c * r
-
-    return distance
-
-
+# def elapsed_time(start, end):
+#     """This function is used to find trip duration"""
+#
+#     start_date = datetime.strptime(start, '%Y-%m-%d %H:%M:%S')
+#     end_date = datetime.strptime(end, '%Y-%m-%d %H:%M:%S')
+#
+#     duration = end_date - start_date
+#     days, seconds = duration.days, duration.seconds
+#     minutes = (seconds % 3600) // 60
+#     return minutes
+#
+#
+# def haversine(lat1, lon1, lat2, lon2):
+#     """
+#     Calculate the great circle distance between two points
+#     on the earth (specified in decimal degrees)
+#     """
+#
+#     # convert decimal degrees to radians
+#     lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+#
+#     # haversine formula
+#     dlon = lon2 - lon1
+#     dlat = lat2 - lat1
+#     a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+#     c = 2 * asin(sqrt(a))
+#     r = 6371  # Radius of earth in kilometers. Use 3956 for miles
+#
+#     distance = c * r
+#
+#     return distance
+#
+#
+# spark.udf.register('elapsed_time', elapsed_time)
+# spark.udf.register('haversine', haversine)
+#
+# trip_data.registerTempTable("trips")
+#
+# res = spark.sql(
+#     "SELECT _c0 as vendor, elapsed_time(_c1, _c2) as duration,haversine(cast(_c3 as float), cast(_c4 as float), cast(_c5 as float), cast(_c6 as float)) as distance from trips")
+#
+# res.registerTempTable("results")
+# groups = spark.sql("SELECT vendor, MAX(distance) FROM results GROUP BY vendor")
+trip_df = spark.read.csv('file:////home/user/project-data/yellow_trip_data/yellow_tripdata_1m.csv')
+vendors = spark.read.csv('file:///home/user/project-data/yellow_trip_data/yellow_tripvendors_1m.csv')
 spark.udf.register('elapsed_time', elapsed_time)
 spark.udf.register('haversine', haversine)
+trip_df.registerTempTable("trips")
+vendors.registerTempTable("vendors")
 
-trip_data.registerTempTable("trips")
+vendor_trips = spark.sql("SELECT vendors._c1 as vendor_id, elapsed_time(trips._c1,trips._c2) as duration, haversine(cast(_c3 as float), cast(_c4 as float), cast(_c5 as float), cast(_c6 as float)) as distance FROM trips INNER JOIN vendors ON trips._c0=vendors._c0 WHERE trips._c3!='0' AND trips._c4!='0' AND trips._c5!='0' AND trips._c6!='0'")
 
-res = spark.sql(
-    "SELECT _c0 as vendor, elapsed_time(_c1, _c2) as duration,haversine(cast(_c3 as float), cast(_c4 as float), cast(_c5 as float), cast(_c6 as float)) as distance from trips")
+vendor_trips.registerTempTable("vendor_trips")
+groups = spark.sql("SELECT vendor_id, MAX(distance) as max_distance FROM vendor_trips GROUP BY vendor_id")
+grp_rdd = groups.rdd.collect()
 
-res.registerTempTable("results")
-groups = spark.sql("SELECT vendor, MAX(distance) FROM results GROUP BY vendor")
+result = spark.sql("SELECT * FROM vendor_trips WHERE vendor_id={} AND distance={} OR vendor_id={} AND distance={}".
+                   format(grp_rdd[0]['vendor_id'], grp_rdd[0]['max(distance)'], grp_rdd[1]['vendor_id'], grp_rdd[1]['max(distance)'])
+                   )
+result.show()
+
+
+
 
