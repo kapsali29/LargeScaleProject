17cfe24bfbb60562e5a04d11733eda362f38569e
konAlexakis <44292879+konAlexakis@users.noreply.github.com>
2020-06-08 14:53:08 +0300

    Update Q2.py

--- part1A/Q2.py
+++ part1A/Q2.py
@@ -29,3 +29,25 @@ class Q2(object):
                    grp_rdd[1]['max_distance'])
             )
         result.show()
+        
+    def mr_api(trip_data,vendors):
+
+        trip_data = sc.textFile(HDFS_TRIP_DATA_PATH). \
+                    filter(lambda x : (float(x.split(",")[3])!=0 and float(x.split(",")[4]!=0
+                        and float(x.split(",")[5]!=0 and float(x.split(",")[6]!=0))))). \
+                    map(lambda x : (x.split(",")[0],(elapsed_time(x.split(",")[1],
+                        x.split(",")[2]),haversine(float(x.split(",")[3]),float(x.split(",")[4]),
+                        float(x.split(",")[5]),float(x.split(",")[6])))))
+
+        vendors = sc.textFile(HDFS_TRIP_VENDORS_PATH). \
+                    map(lambda x : (x.split(",")[0], x.split(",")[1]))
+
+
+        results = trip_data.join(vendors). \
+                    map(lambda x : (x[1][1], [x[1][0][0],x[1][0][1]])). \
+                    reduceByKey(lambda x,y: find_max(x,y)). \
+                    map(lambda x : (x[0],x[1][0],x[1][1]))
+
+
+        for result in results.collect():
+             print(result)
