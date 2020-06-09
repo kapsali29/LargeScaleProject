5ac0e9d3a73c49fe0363df96411a37ec939f415e
konAlexakis <44292879+konAlexakis@users.noreply.github.com>
2020-06-09 23:34:18 +0300

    Update my_partA.py

--- my_partA.py
+++ my_partA.py
@@ -1,7 +1,7 @@
 from pyspark.sql.types import DoubleType
 from datetime import datetime
 from math import radians, cos, sin, asin, sqrt
-from helpers import load_csv_data, init_spark, haversine, elapsed_time
+from helpers import load_csv_data, init_spark, haversine, elapsed_time, find_max
 from settings import TRIP_DATA, HDFS_TRIP_DATA_PATH, PARQUET_TRIP_DATA, DATE_FORMAT
 
 from pyspark.sql import SparkSession
@@ -51,24 +51,21 @@ def find_max(x,y):
     return y
 
 '''
-trip_data = sc.textFile("hdfs://master:9000/yellow_trip_data/yellow_tripdata_1m.csv"). \
-            filter(lambda x : (float(x.split(",")[3])!=0 and float(x.split(",")[4]!=0
-                        and float(x.split(",")[5]!=0 and float(x.split(",")[6]!=0))))). \
-            map(lambda x : (x.split(",")[0],(elapsed_time(x.split(",")[1],
-                        x.split(",")[2]),haversine(float(x.split(",")[3]),float(x.split(",")[4]),
-                        float(x.split(",")[5]),float(x.split(",")[6])))))
+trip_data = sc.textFile("hdfs://master:9000/yellow_trip_data/yellow_tripdata_1m.csv")
+vendors = sc.textFile("hdfs://master:9000/yellow_trip_data/yellow_tripvendors_1m.csv")
 
-vendors = sc.textFile("hdfs://master:9000/yellow_trip_data/yellow_tripvendors_1m.csv"). \
-            map(lambda x : (x.split(",")[0], x.split(",")[1]))
+trip_data = trip_data.map(lambda x : (x.split(",")[0],x.split(",")[1],x.split(",")[2],float(x.split(",")[3]),float(x.split(",")[4]),float(x.split(",")[5]),float(x.split(",")[6])))
 
+filtered_trip_data=trip_data.filter(lambda x : x[3]!=0 and x[4]!=0 and x[5]!=0 and x[6]!=0). \
+                             map(lambda x : (x[0],(elapsed_time(x[1],x[2]),haversine(x[3], x[4], x[5], x[6]))))
 
+vendors = vendors.map(lambda x : (x.split(",")[0], x.split(",")[1]))
 
-result = trip_data.join(vendors). \
-    map(lambda x : (x[1][1], [x[1][0][0],x[1][0][1]])). \
-    reduceByKey(lambda x,y: find_max(x,y)). \
-    map(lambda x : (x[0],x[1][0],x[1][1]))
+result = filtered_trip_data.join(vendors). \
+                            map(lambda x : (x[1][1], [x[1][0][0],x[1][0][1]])). \
+                            reduceByKey(lambda x,y: find_max(x,y)). \
+                            map(lambda x : (x[0],x[1][0],x[1][1]))
 
 
 for i in result.collect():
     print(i)
-
