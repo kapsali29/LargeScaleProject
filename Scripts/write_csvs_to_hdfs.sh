6851824dc16c115af6d0330e3f2a68215c6d57bd
Panagiotis Kapsalis <pkapsalis@epu.ntua.gr>
2020-06-15 18:49:38 +0300

    Some changes on scripts folder

--- Scripts/write_csvs_to_hdfs.sh
+++ Scripts/write_csvs_to_hdfs.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+echo "Move yellow trip data to HDFS"
+hadoop fs -put $HOME/project-data/yellow_trip_data/yellow_tripdata_1m.csv hdfs://master:9000/yellow_trip_data/.
+hadoop fs -put $HOME/project-data/yellow_trip_data/yellow_tripvendors_1m.csv hdfs://master:9000/yellow_trip_data/.
 
-hadoop fs -put ../../project-data/yellow_trip_data/yellow_tripdata_1m.csv hdfs://master:9000/yellow_trip_data/.
-hadoop fs -put ../../project-data/yellow_trip_data/yellow_tripvendors_1m.csv hdfs://master:9000/yellow_trip_data/.
-
-hadoop fs -put ../../project-data/customer_complaints/customer_complaints.csv hdfs://master:9000/customer_complaints/.
+echo "Move customer complaints data to HDFS"
+hadoop fs -put $HOME/project-data/customer_complaints/customer_complaints.csv hdfs://master:9000/customer_complaints/.
