253525870b388862d0bd368cfc8c6d49f6056f82
Panagiotis Kapsalis <pkapsalis@epu.ntua.gr>
2020-06-17 23:48:27 +0300

    some changes on helpers

--- helpers.py
+++ helpers.py
@@ -49,25 +49,41 @@ def date_to_hour(row):
     return hour, latitude, longitude
 
 
-def haversine(lat1, lon1, lat2, lon2):
+# def haversine(lat1, lon1, lat2, lon2):
+#     """
+#     Calculate the great circle distance between two points
+#     on the earth (specified in decimal degrees)
+#     """
+#
+#     # convert decimal degrees to radians
+#     lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+#
+#     # haversine formula
+#     dlon = lon2 - lon1
+#     dlat = lat2 - lat1
+#     a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+#     c = 2 * asin(sqrt(a))
+#     r = 6371  # Radius of earth in kilometers. Use 3956 for miles
+#
+#     distance = c * r
+#
+#     return distance
+
+def haversine(lon1, lat1, lon2, lat2):
     """
     Calculate the great circle distance between two points
     on the earth (specified in decimal degrees)
     """
-
     # convert decimal degrees to radians
     lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
 
     # haversine formula
     dlon = lon2 - lon1
     dlat = lat2 - lat1
-    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
     c = 2 * asin(sqrt(a))
-    r = 6371  # Radius of earth in kilometers. Use 3956 for miles
-
-    distance = c * r
-
-    return distance
+    r = 6371 # Radius of earth in kilometers. Use 3956 for miles
+    return c * r
 
 
 def elapsed_time(start, end):
