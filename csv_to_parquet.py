c385e15a259e66e57a0a3f3c9c3d9aa58e34a464
Panagiotis Kapsalis <pkapsalis@epu.ntua.gr>
2020-07-24 13:41:46 +0300

    Some changes

--- csv_to_parquet.py
+++ csv_to_parquet.py
@@ -6,14 +6,18 @@ from settings import TRIP_DATA,TRIP_VENDORS, PARQUET_TRIP_DATA,PARQUET_TRIP_VEND
 
 spark, sc = init_spark()
 
-start_time = time.time()
+start_time1 = time.time()
 
 trip_data = spark.read.format("csv").option("header", "false").load(TRIP_DATA)
 trip_data.write.parquet(PARQUET_TRIP_DATA, mode='overwrite')
+end_time1 = time.time()
+trip_data_time = end_time1 - start_time1
+print("Time needed for transforming TRIP DATA file to PARQUET: {}".format(trip_data_time))
 
+start_time2 = time.time()
 df_trip_vendors = spark.read.format("csv").option("header", "false").load(TRIP_VENDORS)
 df_trip_vendors.write.parquet(PARQUET_TRIP_VENDORS, mode='overwrite')
+end_time2 = time.time()
+trip_vendors_time = end_time2 - start_time2
 
-end_time = time.time()
-parquet_transformation_time = end_time - start_time
-print("Time needed for transforming CSV files to PARQUET: {}".format(parquet_transformation_time))
+print("Time needed for transforming TRIP VENDORS file to PARQUET: {}".format(trip_vendors_time))
